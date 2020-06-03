09c4d5e1221cb9805eb6c8ad97567af5bd3ed3c9
Cem DOŞ <cem.dos@hotmail.com>
2020-06-03 21:52:36 +0300

    Is Takip Sistemi Merhaba

---
+++ Arch.Core/Constants/StaticParams.cs
@@ -0,0 +1,9 @@
+﻿namespace Arch.Core.Constants
+{
+    public struct StaticParams
+    {
+        public const string Dashboard = "/Dashboard";
+        public const string SifrelemeParametresi = "testfalan";
+        public const string DefaultConnstr = "Data Source=CEMDOS\\SQLEXPRESS;initial Catalog=WorkDB;user id=sa;password=1234;MultipleActiveResultSets=True";
+    }
+}
